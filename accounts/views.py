898408a63657ab84f04b44e48e1f7695183643f3
Jihun-Ha <hajihun76@gmail.com>
2025-04-25 03:34:32 +0900

    Version3.2.3 모바일앱버그수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,25 +1,20 @@
 # accounts/views.py
 from allauth.account.views import LoginView, SignupView
-from blog.utils.device import is_mobile_request
 from django.shortcuts import redirect
+from blog.utils.device import is_mobile_request
+
 
 class CustomLoginView(LoginView):
     def get_template_names(self):
         if is_mobile_request(self.request):
             return ['accounts/mobile_login.html']
         return ['account/login.html']
-    
+
     def form_valid(self, form):
         response = super().form_valid(form)
         if is_mobile_request(self.request):
             return redirect('/busorder/')
         return response
-    
-    def get_success_url(self):
-        # 모바일에서는 /busorder/ 로, 아니면 기본값
-        if is_mobile_request(self.request):
-            return '/busorder/'
-        return super().get_success_url()
 
 
 class CustomSignupView(SignupView):
@@ -27,14 +22,9 @@ class CustomSignupView(SignupView):
         if is_mobile_request(self.request):
             return ['accounts/mobile_signup.html']
         return ['account/signup.html']
-    
+
     def form_valid(self, form):
         response = super().form_valid(form)
         if is_mobile_request(self.request):
             return redirect('/busorder/')
         return response
-    
-    def get_success_url(self):
-        if is_mobile_request(self.request):
-            return '/busorder/'
-        return super().get_success_url()
