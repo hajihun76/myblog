e8da194260dcc547b1748a86f18e42df4288552b
Jihun-Ha <hajihun76@gmail.com>
2025-04-25 14:55:53 +0900

    버그수정

--- busorder/urls.py
+++ busorder/urls.py
@@ -5,7 +5,7 @@ from .views import BusOrderMainView, PermissionPendingView, BusOrderAPI, BusOrde
 app_name = 'busorder'
 
 urlpatterns = [
-    path("", views.calendar_view, name="calendar-view"),
+    path("calendar/", views.calendar_view, name="calendar-view"),
     path("order-check/", views.order_check, name="order-check"),
     path("logs/", views.query_logs, name="query-logs"),  # 개인 전용
     path("logs/all/", views.query_logs_all, name="query-logs-all"),  # 관리자 전용
